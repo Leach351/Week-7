b65f4ab31d7de673e520df58bf4c76a7b815ee09
David <drobinson12@student.waubonsee.edu>
2019-03-13 22:32:04 -0500

    First commit.

---
+++ Week 7.py
@@ -0,0 +1,11 @@
+## Week 7 Dev Struct
+
+
+numOne = float(input("Please enter the first number? : "))
+
+numTwo = float(input("PLease enter the secont number : "))
+
+print ("The non decimal division of those two numbers is ", (numOne // numTwo))
+
+print ("The remainder of the division product is ", ((numOne / numTwo) - (numOne // numTwo))) 
+
