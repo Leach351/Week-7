9334c2e8446ebc02c953d9a52dec0165bfbb2bad
David <drobinson12@student.waubonsee.edu>
2019-03-13 22:37:51 -0500

    This is the commit for Addition.

--- Week 7.py
+++ Week 7.py
@@ -9,3 +9,15 @@ print ("The non decimal division of those two numbers is ", (numOne // numTwo))
 
 print ("The remainder of the division product is ", ((numOne / numTwo) - (numOne // numTwo))) 
 
+
+numThree = float(input("Please enter a third number : "))
+
+numFour = float(input("Please enter a fourt number : "))
+
+print ("The non decimal division of those two numbers is ", (numThree // numFour))
+
+print ("The remainder of the division product is ", ((numThree / numFour) - (numThree // numFour)))
+
+print ("The total of all the numbers is :", (numOne + numTwo + numThree + numFour))
+
+
